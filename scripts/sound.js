b09f89b78c02931e83ca65b739255e69f96007f6
Manu Kumar Pal <102711650+manukumar07@users.noreply.github.com>
2023-06-14 07:44:28 +0530

    Update sound.js

---
+++ scripts/sound.js
@@ -0,0 +1,17 @@
+
+// const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+// const oscillator = audioContext.createOscillator();
+// const audioGainNode = audioContext.createGain();
+    
+//     oscillator.connect(audioGainNode);
+//     audioGainNode.connect(audioContext.destination);
+// function Playsound() {
+//     oscillator.frequency.value = 440;
+//     audioGainNode.gain.value = 0.05;
+
+//     oscillator.start();
+
+//     setTimeout(() => {
+//       oscillator.stop();
+//     }, animationSpeed);
+//   }
